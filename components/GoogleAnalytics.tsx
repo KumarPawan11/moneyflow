8fdd302769ab58f68f632de6f78c36e9c33dc484
Pawan Kumar <pawankumar@Pawans-MacBook-Air.local>
2026-08-09 00:19:04 +0530

    Fix window.gtag assignment and execution in GA4 initialization script

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -44,21 +44,17 @@ export function GoogleAnalytics() {
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
-      <Script
-        id="google-analytics-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              send_page_view: true
-            });
-          `,
-        }}
-      />
+      <Script id="google-analytics-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            send_page_view: true,
+          });
+        `}
+      </Script>
       <Suspense fallback={null}>
         <GoogleAnalyticsTracker />
       </Suspense>
